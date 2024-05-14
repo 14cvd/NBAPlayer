921147598c110c047c0140721895783246ee566d
Cavid Abbasaliyev <cavid.abbasaliyev@kapitalbank.az>
2024-05-14 11:10:24 +0400

    Initial Commit

---
+++ NBAPlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NBAPlayer
+//
+//  Created by cavID on 14.05.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
